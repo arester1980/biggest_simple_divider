3df3feea28391fb1993913cfd8d1183c99903f5f
Arester <temultik@gmail.com>
2020-04-07 16:22:18 +0300

    create main window and button

--- biggest_simple_divider.py
+++ biggest_simple_divider.py
@@ -1,45 +1,37 @@
 import tkinter as tk
 
-class Window(tk.Frame):
-	def __init__(self, master=None):
-		super().__init__(master)
-		self.master = master
-		self.pack()
-		self.create_button()
-		self.create_field()
-		self.create_result()
-				
-	def create_button(self):
-		self.hi_there = tk.Button(self)
-		self.hi_there["text"] = "Найти самый большой простой делитель"
-		self.hi_there["command"] = self.calc
-		self.hi_there.pack(side="top")
-
-	def create_field(self):
-		self.hi_there = tk.Entry(self)
-		self.hi_there["text"] = "Для какого числа?"
-		#self.hi_there["command"] = self.calc
-		self.hi_there.pack(side="bottom")
-
-	def create_result(self):
-		self.hi_there = tk.Label(self)
-		self.hi_there["text"] = "Результат"
-		#self.hi_there["command"] = self.calc
-		#self.hi_there.pack(side="bottom")
-
-	def calc(self):
-		num = int(input('Enter any number: '))
-		z = 1
-		divid = []
-		while z <= num:
-			x = num/z
-			x = str(x)
-			if x[-1::] == '0':
-				divid.append(z)
-				num = num/z
-			z +=1
-		print(f'Max simple divider is {max(divid)}')
-
 root = tk.Tk()
-app = Window(master=root)
-app.mainloop()
+
+
+# def calc(num):
+#     z = 1
+#     divide = []
+#     while z <= num:
+#         x = num / z
+#         x = str(x)
+#         if x[-1::] == '0':
+#             divid.append(z)
+#             num = num / z
+#         z += 1
+#     return max(divide)
+
+def calc(num):
+    return print(num)
+
+
+num_field = tk.Entry()
+calc_butt = tk.Button(
+    text='Посчитать',
+    font=('Helvetica', '12', 'bold'),
+    command=calc(num_field.get())
+)
+lab_entry = tk.Label(text='Введите число', font=('Helvetica', '12'))
+lab_res = tk.Label(text='Результат', font=('Helvetica', '12'))
+res = tk.Label(text=calc, font=('Helvetica', '14', 'bold'))
+calc_butt.grid(row=1, column=3, columnspan=2)
+num_field.grid(row=1, column=2)
+lab_entry.grid(row=1, column=1, sticky='w')
+lab_res.grid(row=2, column=1, sticky='w')
+res.grid(row=2, column=2, sticky='w')
+
+root.mainloop()
